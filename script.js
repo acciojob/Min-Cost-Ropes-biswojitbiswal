8b31ee18ae97b5034e442d0460b48fbf9b579ded
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-19 11:21:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if (arr.length < 2) {
+        return 0;
+    }
+    
+
+    const minHeap = arr.slice();
+    minHeap.sort((a, b) => a - b);
+
+    let totalCost = 0;
+
+    while (minHeap.length > 1) {
+
+        const first = minHeap.shift();
+        const second = minHeap.shift();
+
+        const cost = first + second;
+        totalCost += cost;
+
+        
+        minHeap.push(cost);
+        minHeap.sort((a, b) => a - b);
+
+    return totalCost;
   
 }
 
