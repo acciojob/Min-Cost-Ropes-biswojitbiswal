44b954b4722d57d2c537c5eef5533c2455793af9
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-23 20:28:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function minCost(arr){
+				if (arr.length <= 1) return 0;
 
+			    let heap = [...arr].sort((a, b) => a - b);
+			    let totalCost = 0;
+			
+			    while (heap.length > 1) {
+			        let firstMin = heap.shift();
+			        let secondMin = heap.shift();
+			        
+			        let newRope = firstMin + secondMin;
+			        totalCost += newRope;
+			
+			        heap.push(newRope);
+			        heap.sort((a, b) => a - b); 
+			    }
+			
+			    return totalCost;
+			}
+
+		alert(minCost(arr));
